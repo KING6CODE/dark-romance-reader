6887cfcec6fdebf64a893e2bc535dc22ade12361
KING6CODE <alistpassecoutrin@gmail.com>
2026-07-08 11:58:58 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,16 +16,28 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
+        {/* Google Ads */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-18003821360"
           strategy="afterInteractive"
         />
-        <Script id="google-ads-init" strategy="afterInteractive">
+        <Script id="google-ads" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-18003821360');
+          `}
+        </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xj6l8ejn8z");
           `}
         </Script>
       </head>
